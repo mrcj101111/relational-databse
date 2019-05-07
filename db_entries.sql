1afe3427129e82ae1ceeab84d0a413d3831f599d
mrcj101111 <cornebotma@hotmail.com>
2019-05-07 09:16:32 +0200

    Added inactive function

--- db_entries.sql
+++ db_entries.sql
@@ -1,9 +1,9 @@
     (1, to_timestamp('16-05-2019 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 100, True),
     (2, to_timestamp('16-05-2015 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
-    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 500, True),
+    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 0, True),
     (3, to_timestamp('16-05-2019 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
-    '16-06-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 1000, False),
+    '16-06-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 5, False),
     (4, to_timestamp('16-05-2017 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 100, True),
     (5, to_timestamp('16-05-2015 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
@@ -21,11 +21,11 @@
     (11, to_timestamp('16-12-2011 13:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 100, True),
     (12, to_timestamp('16-11-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
-    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 100, True),
+    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 3, True),
     (13, to_timestamp('17-05-2011 15:50:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 800, True),
     (14, to_timestamp('16-05-2012 15:50:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
-    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 1000, False),
+    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 1, False),
     (15, to_timestamp('28-05-2000 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 5000, False),
     (16, to_timestamp('26-05-2015 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
@@ -33,8 +33,8 @@
     (17, to_timestamp('16-05-2015 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 600, True),
     (18, to_timestamp('16-05-2013 18:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
-    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 100, False),
+    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 2, False),
     (19, to_timestamp('16-05-2011 20:40:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
-    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 200, True),
+    '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 1, True),
     (20, to_timestamp('16-05-2012 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), to_timestamp(
     '16-05-2011 15:36:38', 'dd-mm-yyyy hh24:mi:ss'), 'edit file', 100, True)
--- rd_assignment.py
+++ rd_assignment.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import argparse
+import sys
 import csv
 
 
@@ -56,10 +57,33 @@ class SqlQueries:
                                       host='localhost', password=args.password)
         cursor = connection.cursor()
         sql = "COPY (SELECT * FROM products ORDER BY amount DESC) TO STDOUT WITH CSV HEADER DELIMITER ','"
-        with open('amount_acs.csv', 'w') as file:
+        with open('highest_amount_acs.csv', 'w') as file:
+            cursor.copy_expert(sql, file)
+
+    def active_and_above_10(self):
+        connection = psycopg2.connect(dbname=args.db_name, user=args.username,
+                                      host='localhost', password=args.password)
+        cursor = connection.cursor()
+        sql = "COPY (SELECT * FROM products WHERE amount > 10 AND is_active = TRUE)" \
+              "TO STDOUT WITH CSV HEADER DELIMITER ','"
+        with open('active_and_above_10.csv', 'w') as file:
+            cursor.copy_expert(sql, file)
+
+    def inactive(self):
+        connection = psycopg2.connect(dbname=args.db_name, user=args.username,
+                                      host='localhost', password=args.password)
+        cursor = connection.cursor()
+        sql = "COPY (SELECT description, amount, is_active FROM products WHERE is_active = FALSE) TO STDOUT WITH CSV HEADER DELIMITER ','"
+        with open('inactive.csv', 'w') as file:
             cursor.copy_expert(sql, file)
 
 
+print('Are you connecting to an existing db?')
+input = input()
+if input == 'yes':
+    print('true')
+else:
+    print('false')
 parser = argparse.ArgumentParser()
 parser.add_argument('-username', action='store', dest='username', help='username', required=True)
 parser.add_argument('-password', action='store', dest='password', help='password', required=True)
@@ -70,5 +94,7 @@ args = parser.parse_args()
 #execute_sql_file = SqlQueries().read_sql_file(connection=initialize)
 #add_column = SqlQueries().add_column()
 #add_entries = SqlQueries().write_to_tabel()
+
 #print_out = SqlQueries().stdout_print_out()
-highest_amount = SqlQueries().write_highest_amount()
+#highest_amount = SqlQueries().write_highest_amount()
+active_and_above_10 = SqlQueries().inactive()
